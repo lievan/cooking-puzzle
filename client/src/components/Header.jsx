4b293153a497397a802a80c462d7ae8fca12975e
lievan <0evanli0@gmail.com>
2024-05-05 22:26:58 -0400

    ingreds

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header(props) {
   return (
     <header>
       <h1>Cooking Puzzle Game</h1>
-      <p>Add ingredients and subtract ingredients to your AI generated dish</p>
+      <p>Add and subtract ingredients to your AI generated dish</p>
     </header>
   );
 }
